69af92a6b0e410851e23891c5abffa682328bc6b
Gabriela Susana Mendez Barrera <74455872+GabyArgu@users.noreply.github.com>
2022-02-27 13:01:04 -0600

    Jefryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

---
+++ js/gridclientes.js
@@ -0,0 +1,24 @@
+$('#owl1').owlCarousel({
+    loop:true,
+    margin: 5,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout:2500,
+    autoplayHoverPause: true,
+    nav: true,
+    dots:false,
+    responsive: {
+        0:{
+            items: 1,
+        },
+        768:{
+            items: 2,
+        },
+        1100:{
+            items: 3,
+        },
+        1400:{
+            items: 4,
+        }
+    }
+});
