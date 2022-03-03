c7ea321e7ec38e414c8d009ac890c3c8e4525fb0
Gabriela Susana Mendez Barrera <74455872+GabyArgu@users.noreply.github.com>
2022-03-03 03:26:25 -0600

    Privado

---
+++ Privado/js/dashboard.js
@@ -0,0 +1,53 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          19489,
+          24092,
+          22034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#FFCCCC',
+        borderWidth: 4,
+        pointBackgroundColor: '#FF97CC'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
