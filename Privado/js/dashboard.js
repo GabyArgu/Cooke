0579c83e7d6d0ca37cbfd826830c1fa28dd6d485
Jefry224 <jefry.panameno02@gmail.com>
2022-03-07 09:42:45 -0600

    correción sidebar

--- Privado/js/dashboard.js
+++ Privado/js/dashboard.js
@@ -1,13 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelectorAll('.sidebar .sidebar-link').forEach(function(element){
+    
+    element.addEventListener('click', function (e) {
+
+      let nextEl = element.nextElementSibling;
+      let parentEl  = element.parentElement;	
+
+        if(nextEl) {
+            e.preventDefault();	
+            let mycollapse = new bootstrap.Collapse(nextEl);
+            
+            if(nextEl.classList.contains('show')){
+              mycollapse.hide();
+            } else {
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if(opened_submenu){
+                  new bootstrap.Collapse(opened_submenu);
+                }
+            }
+        }
+    }); // addEventListener
+  }) // forEach
+}); 
 
 sidebar = document.getElementById('sidebarMenu');
 function sidebarToggle ()
 {
-  if (window.pageXOffset = 250) {
-    sidebar.classList.add('toggled');
+  if (sidebar.classList.contains('hide')) {
+    sidebar.classList.add('show');
+    sidebar.classList.remove('hide');
   } else {
-    sidebar.classList.remove('toggled');
+    sidebar.classList.add('hide');
+    sidebar.classList.remove('show');
   }
-  
 }
 
 /* globals Chart:false, feather:false */
