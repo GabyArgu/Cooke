84c1f464adac5d49d38fad8bb692dc63cdd7eb3a
jefryvs <jefry.panameno02@gmail.com>
2022-05-04 23:03:36 -0600

    login y primer usuarios, finalizado

--- controllers/privado/crear.js
+++ controllers/privado/crear.js
@@ -1,2 +1,52 @@
+// Constante para establecer la ruta y parámetros de comunicación con la API.
 const API_USUARIOS = SERVER + 'private/usuarios.php?action=';
 
+// Eventos que se ejecutan cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', function () {
+    // Petición para consultar si existen usuarios registrados.
+    fetch(API_USUARIOS + 'readUsers', {
+        method: 'get'
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Se comprueba si existe una sesión, de lo contrario se revisa si la respuesta es satisfactoria.
+                if (response.session) {
+                    location.href = 'dashboard.html';
+                } else if (response.status) {
+                    sweetAlert(3, response.message, 'index.html');
+                } else {
+                    sweetAlert(4, 'Debe crear un usuario para comenzar', null);
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    });
+});
+
+
+// Método manejador de eventos que se ejecuta cuando se envía el formulario de registrar.
+document.getElementById('register-form').addEventListener('submit', function (event) {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Petición para registrar el primer usuario del sitio privado.
+    fetch(API_USUARIOS + 'register-first-user', {
+        method: 'post',
+        body: new FormData(document.getElementById('register-form'))
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                if (response.status) {
+                    sweetAlert(1, response.message, 'index.html');
+                } else {
+                    sweetAlert(2, response.exception, null);
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    });
+});
