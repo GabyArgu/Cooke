47b92efd5fea2b03a1c5e4e6cbe7d7fe3e68fa88
Gabriela Susana Mendez Barrera <74455872+GabyArgu@users.noreply.github.com>
2022-05-16 13:28:51 -0600

    Creación de crud categoria de productos
    Con fe y funciona, search algun dia estara si jefry lo pone

---
+++ api/models/categorias_productos.php
@@ -0,0 +1,139 @@
+<?php
+/*
+*	Clase para manejar la tabla catalogo de colores de la base de datos de la tienda.
+*   Es una clase hija de Validator.
+*/
+class categoriaCP extends Validator
+{
+    // Declaración de atributos (propiedades).
+    private $id = null;
+    private $nombre = null;
+    private $descripcion = null;
+    private $estado = null;
+
+    /*
+    *   Métodos para validar y asignar valores de los atributos.
+    */
+    public function setId($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setNombre($value)
+    {
+        if ($this->validateAlphanumeric($value, 1, 50)) {
+            $this->nombre = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setDescripcion($value)
+    {
+        if($this->validateString($value, 1, 250)) {
+            $this->descripcion = $value;
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    public function setEstado($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->estado = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function readAll()
+    {
+        $sql = 'SELECT * from "categoriaProducto"';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT *
+        FROM "categoriaProducto"
+        where "idCategoria" = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+    */
+    /* SEARCH */
+    public function searchRows($value)
+    {
+        $sql = 'SELECT "nombreCategoriaP"
+                FROM "categoriaProducto"
+                WHERE "categoriaProducto" ILIKE ? ';
+        $params = array("%$value%");
+        return Database::getRows($sql, $params);
+    }
+
+    /* CREATE */
+    public function createRow()
+    {
+        $sql = 'INSERT INTO "categoriaProducto"("nombreCategoriaP", "descripcionCategoria", estado)
+                VALUES (?,?, 1);';
+        $params = array($this->nombre, $this->descripcion);
+        return Database::executeRow($sql, $params);
+    }
+
+
+    /* UPDATE */
+    public function updateRow()
+    {
+        $sql = 'UPDATE "categoriaProducto"
+                SET "nombreCategoriaP" = ?, "descripcionCategoria"=?,"estado" = ?
+                WHERE "idCategoria" = ?';
+            $params = array($this->nombre,$this->descripcion,$this->estado,$this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    /* DELETE */
+    /* Función para borrar un nombre de la base (Solo se inahbilita)*/
+    public function deleteRow()
+    {
+        $sql = 'UPDATE "categoriaProducto"
+                SET estado = 2
+                WHERE "idCategoria" = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+}
---
+++ api/private/categorias_productos.php
@@ -0,0 +1,107 @@
+<?php
+require_once('../helpers/database.php');
+require_once('../helpers/validaciones.php');
+require_once('../models/categorias_productos.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $categoria = new categoriaCP;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'exception' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['id_usuario'])) {
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readAll':
+                if ($result['dataset'] = $categoria->readAll()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            case 'search':
+                $_POST = $categoria->validateForm($_POST);
+                if ($_POST['search'] == '') {
+                    $result['exception'] = 'Ingrese un valor para buscar';
+                } elseif ($result['dataset'] = $categoria->searchRows($_POST['search'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Valor encontrado';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay coincidencias';
+                }
+                break;
+            case 'create':
+                $_POST = $categoria->validateForm($_POST);
+                if (!$categoria->setNombre($_POST['nombreCP'])) {
+                    $result['exception'] = 'Nombre de categoria inválido';
+                }elseif (!$categoria->setDescripcion($_POST['descripcionCP'])) {
+                    $result['exception'] = 'Descripción inválida';
+                } elseif ($categoria->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Categoria de productos agregada correctamente.';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'readOne':
+                if (!$categoria->setId($_POST['id'])) {
+                    $result['exception'] = 'Error con el ID';
+                } elseif ($result['dataset'] = $categoria->readOne()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'Categoria de productos inexistente';
+                }
+                break;
+            case 'update':
+                $_POST = $categoria->validateForm($_POST);
+                if (!$categoria->setId($_POST['u_idCP'])) {
+                    $result['exception'] = 'ID inválido';
+                } elseif (!$data = $categoria->readOne()) {
+                    $result['exception'] = 'Categoria inexistente';
+                } elseif (!$categoria->setNombre($_POST['u_nombreCP'])) {
+                    $result['exception'] = 'Nombre de categoria inválido';
+                }elseif (!$categoria->setDescripcion($_POST['u_descripcionCP'])) {
+                    $result['exception'] = 'Nombre de categoria inválido';
+                } elseif (!$categoria->setEstado($_POST['u_estado'])) {
+                    $result['exception'] = 'Estado inválido';
+                } elseif ($categoria->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Categoria modificada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'delete':
+                if (!$categoria->setId($_POST['id'])) {
+                    $result['exception'] = 'Categoría incorrecta';
+                } elseif (!$data = $categoria->readOne()) {
+                    $result['exception'] = 'Categoría inexistente';
+                } elseif ($categoria->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Categoría eliminada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
+    print(json_encode('Recurso no disponible'));
+}
