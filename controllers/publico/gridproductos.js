1417368a22126cf52bced2549e7f0a625faebf85
Jefry224 <jefry.panameno02@gmail.com>
2022-05-31 10:35:28 -0600

    Subcategorias público finnalizado

--- controllers/publico/gridproductos.js
+++ controllers/publico/gridproductos.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl, {'customClass': 'custom-tooltip'})
     })
   });
 
-function hoverImagen(ruta){
-  document.getElementById("main-img").src=ruta;
-}
+// function hoverImagen(ruta){
+//   document.getElementById("main-img").src=ruta;
+// }
