7f6f0ac8eec362f1c936e00142d41048ce06109d
Jefry224 <jefry.panameno02@gmail.com>
2022-07-20 16:21:42 -0600

    Gráfica funcionando y validada

--- controllers/privado/dashboard.js
+++ controllers/privado/dashboard.js
@@ -5,7 +5,17 @@ const API_PRODUCTOS = SERVER + 'private/productos.php?action=';
 document.addEventListener('DOMContentLoaded', function () {
     // Se llaman a la funciones que generan los gráficos en la página web.
     graficoLineasVentasSemanales();
+    getWeek();
 });
+
+function getWeek() {
+    let today = new Date();
+    let fechas = [];
+    for (let i = 1; i < 8; i++) {
+        fechas.push(today.getDate() - i);
+    }
+    return fechas.reverse();
+}
 
 /*Graficos*/
 /*Ingresos totales por dia en la semana*/
@@ -21,13 +31,18 @@ function graficoLineasVentasSemanales() {
                 // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
                 if (response.status) {
                     // Se declaran los arreglos para guardar los datos a graficar.
-                    let dias = [];
+                    let dias = getWeek();
                     let cantidades = [];
-                    // Se recorre el conjunto de registros devuelto por la API (dataset) fila por fila a través del objeto row.
-                    response.dataset.map(function (row) {
-                        // Se agregan los datos a los arreglos.
-                        dias.push(row.Fecha);
-                        cantidades.push(row.total);
+                    //Iteramos por cada elemento del array que contiene la semana pasada
+                    dias.forEach((dia) => {
+                        //buscamos elemento que coincida con el día para validar existencia de este en el dataset, en caso de no existir retorna undefined
+                        const registro = response.dataset.find((row) => row.Fecha == dia);
+                        let total = 0;
+                        if (registro != undefined) {
+                            total = registro.total;
+                        }
+                        //const total = registro ? registro.total : 0;
+                        cantidades.push(total);
                     });
                     // Se llama a la función que genera y muestra un gráfico de barras. Se encuentra en el archivo components.js
                     lineGraph('vsemana', dias, cantidades, 'Ventas semanales');
@@ -51,14 +66,14 @@ function graficoLineasVentasSemanales() {
             request.json().then(function (response) {
                 // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
                 if (response.status) {
-                    //Validando cuando no hay valores
-                    if(isNaN(parseFloat(response.dataset.total))){
+                    //Le asignamos el valor a la etiqueta del monto
+                    if (isNaN(parseFloat(response.dataset.total))) {
                         document.getElementById('e-semana').innerText = "Todavía no hay ventas esta semana";
                     }
-                    else{
+                    else {
                         document.getElementById('e-semana').innerText = `$${parseFloat(response.dataset.total).toFixed(2)}`;
                     }
-                    
+
                 } else {
                     // Se presenta un mensaje de error cuando no existen datos para mostrar.
                     sweetAlert(4, response.exception, null);
