c095d0630c73edc4255376ee712f95be0adac234
jefryvs <jefry.panameno02@gmail.com>
2022-07-22 14:46:34 -0600

    Gráfica grande funcionando

--- controllers/privado/dashboard.js
+++ controllers/privado/dashboard.js
@@ -1,11 +1,12 @@
 // Constante para establecer la ruta y parámetros de comunicación con la API.
-const API_PRODUCTOS = SERVER + 'private/productos.php?action=';
-
+const API_PRODUCTOS = SERVER + "private/productos.php?action=";
+let multi = {};
 // Método manejador de eventos que se ejecuta cuando el documento ha cargado.
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     // Se llaman a la funciones que generan los gráficos en la página web.
     graficoLineasVentasSemanales();
-    getWeek();
+    graficoMultiVentasCategoria();
+
 });
 
 function getWeek() {
@@ -22,8 +23,8 @@ function getWeek() {
 // Función para mostrar la cantidad de productos por categoría en un gráfico de barras.
 function graficoLineasVentasSemanales() {
     // Petición para obtener los datos del gráfico.
-    fetch(API_PRODUCTOS + 'ventasPorSemana', {
-        method: 'get'
+    fetch(API_PRODUCTOS + "ventasPorSemana", {
+        method: "get",
     }).then(function (request) {
         // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
         if (request.ok) {
@@ -45,20 +46,20 @@ function graficoLineasVentasSemanales() {
                         cantidades.push(total);
                     });
                     // Se llama a la función que genera y muestra un gráfico de barras. Se encuentra en el archivo components.js
-                    lineGraph('vsemana', dias, cantidades, 'Ventas semanales');
+                    lineGraph("vsemana", dias, cantidades, "Ventas semanales (USD)");
                 } else {
-                    document.getElementById('vsemana').remove();
+                    document.getElementById("vsemana").remove();
                     console.log(response.exception);
                 }
             });
         } else {
-            console.log(request.status + ' ' + request.statusText);
+            console.log(request.status + " " + request.statusText);
         }
     });
 
     //Petición para obtener la estadistica de ventas totales por semana
-    fetch(API_PRODUCTOS + 'ventasPorSemanaEstadistica', {
-        method: 'get'
+    fetch(API_PRODUCTOS + "ventasPorSemanaEstadistica", {
+        method: "get",
     }).then(function (request) {
         // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje en la consola indicando el problema.
         if (request.ok) {
@@ -68,19 +69,69 @@ function graficoLineasVentasSemanales() {
                 if (response.status) {
                     //Le asignamos el valor a la etiqueta del monto
                     if (isNaN(parseFloat(response.dataset.total))) {
-                        document.getElementById('e-semana').innerText = "Todavía no hay ventas esta semana";
+                        document.getElementById("e-semana").innerText =
+                            "Todavía no hay ventas esta semana";
+                    } else {
+                        document.getElementById("e-semana").innerText = `$${parseFloat(
+                            response.dataset.total
+                        ).toFixed(2)}`;
                     }
-                    else {
-                        document.getElementById('e-semana').innerText = `$${parseFloat(response.dataset.total).toFixed(2)}`;
-                    }
-
                 } else {
                     // Se presenta un mensaje de error cuando no existen datos para mostrar.
                     sweetAlert(4, response.exception, null);
                 }
             });
         } else {
-            console.log(request.status + ' ' + request.statusText);
+            console.log(request.status + " " + request.statusText);
         }
     });
+}
+
+function graficoMultiVentasCategoria() {
+    let multi = {};
+    Promise.all([
+        fetch(API_PRODUCTOS + "ventasEnSemanaCategoria1"),
+        fetch(API_PRODUCTOS + "ventasEnSemanaCategoria2"),
+        fetch(API_PRODUCTOS + "ventasEnSemanaCategoria3"),
+        fetch(API_PRODUCTOS + "ventasEnSemanaCategoria4"),
+    ])
+        .then(function (responses) {
+            return Promise.all(
+                responses.map(function (response) {
+                    return response.json();
+                })
+            );
+        })
+        .then(function (data) {
+            let dias = getWeek();
+            //Recorremos cada fila del data
+            data.forEach((col, index) =>{
+                //Vaciamos la variable cantidades para llenarla por cada dataset
+                let cantidades = [];
+                //Iteramos por cada elemento del array que contiene la semana pasada
+                dias.forEach((dia) => {
+                    //Buscamos elemento que coincida con el día para validar existencia de este en el dataset, en caso de no existir retorna undefined
+                    const registro = data[index].dataset.find((row) => row.Fecha == dia);
+                    let total = 0;
+                    if (registro != undefined) {
+                        total = registro.total;
+                    }
+                    //const total = registro ? registro.total : 0;
+                    cantidades.push(total);
+                });
+                //Guardamos el array obtenido en el objeto.
+                multi['datos'+col] = cantidades;
+            });
+            //Guardamos todos los legend/label a utilizar (nombre de datos asociados a cada array de datos)
+            multi.categoria1Legend = "Cocina (USD)";
+            multi.categoria2Legend = "Utensilios (USD)";
+            multi.categoria3Legend = "Electrodomésticos (USD)";
+            multi.categoria4Legend = "Recetas (USD)";
+        }).then(()=>{
+            //Una vez se realiza todo lo anterior, se llama la función para generar el gráfico y pasarle los datos.
+            multiLineGraph('chart-ventas-categoria', getWeek(), multi.datos1, multi.datos2, multi.datos3, multi.datos4, multi.categoria1Legend, multi.categoria2Legend, multi.categoria3Legend, multi.categoria4Legend);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
