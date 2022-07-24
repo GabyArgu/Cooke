7f77d96584e731e445807257febb595c2f125f72
jefryvs <jefry.panameno02@gmail.com>
2022-07-23 21:17:24 -0600

    Reporte de factura en sitio público listo

--- api/reports/private/ventas_dia.php
+++ api/reports/private/ventas_dia.php
@@ -20,10 +20,10 @@ if ($datapedidos = $pedido->reportPedidosDelDia()) {
     $pdf->cell(37, 10, utf8_decode('Número de pedido'), 1, 0, 'C', 1);
     $pdf->cell(38, 10, utf8_decode('Nombres del cliente'), 1, 0, 'C', 1);
     $pdf->cell(37, 10, utf8_decode('Apellidos del cliente'), 1, 0, 'C', 1);
-    $pdf->cell(37, 10, utf8_decode('Monto (US$)'), 1, 0, 'C', 1);
+    $pdf->cell(37, 10, utf8_decode('Monto'), 1, 0, 'C', 1);
     $pdf->cell(37, 10, utf8_decode('Fecha'), 1, 1, 'C', 1);
 
-    // Se establece un color de relleno para mostrar el nombre de la categoría.
+    // Se establece un color de relleno para mostrar los registros
     $pdf->setFillColor(225);
     // Se establece la fuente para los datos de los productos.
     $pdf->setFont('Times', '', 11);
@@ -34,12 +34,12 @@ if ($datapedidos = $pedido->reportPedidosDelDia()) {
         $pdf->cell(37, 10, $rowpedido['idPedido'], 1, 0, 'C');
         $pdf->cell(38, 10, utf8_decode($rowpedido['nombresCliente']), 1, 0, 'C');
         $pdf->cell(37, 10, utf8_decode($rowpedido['apellidosCliente']), 1, 0, 'C');
-        $pdf->cell(37, 10, $rowpedido['montoTotal'], 1, 0,'C');
+        $pdf->cell(37, 10, '$'.$rowpedido['montoTotal'], 1, 0,'C');
         $pdf->cell(37, 10, $rowpedido['fechaPedido'], 1, 1, 'C');
     }
     if ($totalVentas = $pedido->reportPedidosDelDiaEstadistica()){
-        $pdf->cell(93, 10, utf8_decode('Total de ventas'), 1, 0, 'C');
-        $pdf->cell(93, 10, $totalVentas['totalVentas'], 1, 0, 'C');
+        $pdf->cell(93, 10, utf8_decode('Monto total de ventas'), 1, 0, 'C');
+        $pdf->cell(93, 10, ('$'.$totalVentas['totalVentas']), 1, 0, 'C');
     }
     else{
         $pdf->cell(0, 10, utf8_decode('No hay total de ventas para mostrar'), 1, 1);
