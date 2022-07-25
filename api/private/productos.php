98168152712c58a1346e17b89661114b20dd6ec8
EyleenSantos <85887967+EyleenSantos@users.noreply.github.com>
2022-07-24 23:42:43 -0600

    case de productoStockCategoria

--- api/private/productos.php
+++ api/private/productos.php
@@ -237,6 +237,14 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'No hay datos disponibles';
                 }
                 break;
+            case 'productoStockCategoria':
+                if ($result['dataset'] = $productos->productoStockCategoria()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['exception'] = 'No hay datos disponibles';
+                }
+                break;
+    
             default:
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
